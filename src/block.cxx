458171e60adec02659473722b3b9a35538eb7e8e
Anthony Templeton <twantempleton11@gmail.com>
2021-08-20 19:51:25 -0400

    tidy up

--- src/block.cxx
+++ src/block.cxx
@@ -1,10 +1,11 @@
 #include <algorithm>
 #include <string>
 #include <sstream>
+#include <utility>
 #include <block.h>
 #include <events.h>
 
-Block::Block(std::string this_block): block(this_block) {
+Block::Block(std::string this_block): block(std::move(this_block)) {
     Block::set_block(block);
 }
 
@@ -18,10 +19,10 @@ void Block::set_block(std::string block) {
 }
 
 void Block::set_event_name(std::string evt_name) {
-    event_name = evt_name;
+    event_name = std::move(evt_name);
 }
 
-void Block::set_description(std::string desc) {
+void Block::set_description(const std::string& desc) {
     auto it = events.find(desc);
     description = it->second;
 }
@@ -69,7 +70,7 @@ std::string Block::get_event_data() {
     if (!event_data.empty() && isspace(event_data.at(event_data.size() - 1))) {
         event_data = event_data.substr(0, event_data.length() - 1);
     }
-    return "{\"event-data\":\"" + event_data + "\"}";
+    return R"({"event_data":")" + event_data + "\"}";
 }
 
 std::string Block::get_all_info() {
